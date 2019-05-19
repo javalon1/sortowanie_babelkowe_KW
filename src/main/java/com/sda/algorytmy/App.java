805073aabfceae4db059575bd0ca435420dee52d
Karolina Wanat <carolinawanat@gmail.com>
2019-05-19 09:29:27 +0100

    sorted

--- src/main/java/com/sda/algorytmy/App.java
+++ src/main/java/com/sda/algorytmy/App.java
@@ -1,13 +1,28 @@
 package com.sda.algorytmy;
 
-/**
- * Hello world!
- *
- */
+import java.util.Arrays;
+
+
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ){
+
+        int[] tab = new int[]{2,1,4,5};
+        int tempo = 0;
+
+        for (int i = 0; i < tab.length - 1; i++) {
+            if (tab[i] > tab[i +1]){
+                tempo = tab[i];
+                tab[i] = tab[i+1];
+                tab[i +1] = tempo;
+            }
+
+        }
+        for (int i : tab) {
+            System.out.print(i + " ");
+        }
+
     }
-}
+
+    }
+
