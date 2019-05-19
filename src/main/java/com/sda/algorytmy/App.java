da116d3bfafbbd45a45b211d56cc9e7e7e26f15d
Karolina Wanat <carolinawanat@gmail.com>
2019-05-19 11:14:29 +0100

    sorted + random int

--- src/main/java/com/sda/algorytmy/App.java
+++ src/main/java/com/sda/algorytmy/App.java
@@ -1,28 +1,61 @@
 package com.sda.algorytmy;
 
 import java.util.Arrays;
+import java.util.Random;
 
 
-public class App 
-{
-    public static void main( String[] args ){
+public class App {
+    public static void main(String[] args) {
 
-        int[] tab = new int[]{2,1,4,5};
-        int tempo = 0;
 
-        for (int i = 0; i < tab.length - 1; i++) {
-            if (tab[i] > tab[i +1]){
-                tempo = tab[i];
-                tab[i] = tab[i+1];
-                tab[i +1] = tempo;
+        Random random = new Random();
+        int max = 200;
+        int min = -1000;
+
+
+        int[] tab = new int[20];
+        long startTime = System.currentTimeMillis();
+
+        for (int i = 0; i < tab.length; i++) {
+             int randomMunber = random.nextInt(max - min + 1) - max;
+            tab[i] = randomMunber;
+
+        }
+        //przed sortowaniem
+        System.out.println(Arrays.toString(tab));
+
+        long endTime = System.currentTimeMillis();
+        long totaTime = endTime - startTime;
+        System.out.println("Czas wykonania  " + totaTime);
+        startTime = System.currentTimeMillis();
+        sort(tab);
+        endTime = System.currentTimeMillis();
+        totaTime = endTime - startTime;
+        System.out.println("czas sortowania " + totaTime);
+
+        //po sortowaniu
+        System.out.println(Arrays.toString(tab));
+
+
+
+    }
+    public static int[] sort(int[] tab){
+        int temp =0;
+
+        for (int j = 0; j < tab.length ; j++) {
+
+            for (int i = 0; i < tab.length - 1; i++) {
+                if (tab[i] > tab[i + 1]) {
+                    temp = tab[i + 1];
+                    tab[i + 1] = tab[i];
+                    tab[i] = temp;
+                }
+
             }
-
         }
-        for (int i : tab) {
-            System.out.print(i + " ");
-        }
-
+        return tab;
     }
 
     }
+
 
